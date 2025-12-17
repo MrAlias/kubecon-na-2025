fd2de2d47d8cce792fba609b95e10e7804070955
Tyler Yahn <codingalias@gmail.com>
2025-12-17 13:09:38 -0800

    Update the apps script

--- demo/apps.sh
+++ demo/apps.sh
@@ -2,4 +2,5 @@ kind load docker-image details:local
 kind load docker-image reviews:local
 kind load docker-image ratings:local
 kind load docker-image ratings-v2:local
+kind load docker-image users:local
 kubectl apply -f bookinfo.yaml
